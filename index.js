faf66328342b64e5b16f126fe823fc9e57731274
Rafael Finol <rafaelfinol88@gmail.com>
2023-03-11 13:33:26 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Rafael",
+    streetAddress: "123",
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, rafa){
+    const modifyName = {...employee};
+    modifyName[name] = rafa;
+    return modifyName;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(){
+    const deleteEmployee = {...employee};
+    delete deleteEmployee.name;
+    return deleteEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee){
+    delete employee.name;
+    return employee;
+}
